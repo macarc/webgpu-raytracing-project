fb61ea27698378a4ff81342be79f0535966e552c
macarc <archiepiper@hotmail.com>
2026-01-11 13:35:12 +0000

    M5: add very basic UI allowing user to adjust ray-tracing settings
    The Mithril JS virtual DOM library is used.
    
    Just supports modifying ray count and number of bounces. In future
    this will allow assigning materials, etc.
    
    HOWEVER, this has already uncovered strange behaviour as it seems that
    the audio is not affected by how many bounces are calculated (even
    a single bounce sounds pretty much identical - and yes, it is
    definitely only calculating a single bounce, as it runs much faster).

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,101 @@
 import { rayTrace, Settings } from "./ray_tracing";
 import { SAMPLE_RATE } from "./constants";
 import { CUBE_FACES } from "./geometry_data";
+import m from "mithril";
 
-let audioToPlay = new Float32Array();
-let ctx: AudioContext | null = null;
-
-async function withDisabled(
-  element: HTMLButtonElement,
-  fn: () => Promise<void>,
-) {
-  const text = element.innerText;
-  element.innerText = "Running";
-  element.disabled = true;
-  await fn();
-  element.innerText = text;
-  element.disabled = false;
-}
-
-const RAY_TRACING_SETTINGS: Settings = {
+let state = {
   rayCount: 20000,
   maxBounces: 20000,
   geometry: CUBE_FACES,
+  audioToPlay: null as Float32Array | null,
+  ctx: null as AudioContext | null,
+  running: false,
+
+  runRaytracing: async function () {
+    state.running = true;
+    state.audioToPlay = await rayTrace(state);
+    state.running = false;
+  },
+
+  playAudio: function () {
+    // If no ray-tracing has happened, ignore.
+    if (!state.audioToPlay) {
+      return;
+    }
+
+    // Create an AudioContext if one does not exist.
+    if (!state.ctx) {
+      state.ctx = new AudioContext();
+    }
+
+    // Create the buffer to play.
+    const sourceBuffer = state.ctx.createBuffer(
+      1,
+      state.audioToPlay.length,
+      SAMPLE_RATE,
+    );
+    const channel0 = sourceBuffer.getChannelData(0);
+    for (let i = 0; i < state.audioToPlay.length; ++i) {
+      channel0[i] = state.audioToPlay[i];
+    }
+
+    // Create the audio buffer source to play.
+    const source = state.ctx.createBufferSource();
+    source.buffer = sourceBuffer;
+
+    // Start playing the audio buffer source.
+    source.connect(state.ctx.destination);
+    source.start(0);
+  },
+};
+
+let AppView = {
+  view: function () {
+    return m("div", [
+      m("section", { style: "border:1px solid black;" }, [
+        m("label.block", [
+          "Ray count:",
+          m("input", {
+            type: "number",
+            min: "1",
+            value: state.rayCount,
+            oninput: function (e: InputEvent) {
+              state.rayCount = parseInt((e.target as HTMLInputElement).value);
+            },
+          }),
+        ]),
+        m("label.block", [
+          "Number of bounces:",
+          m("input", {
+            type: "number",
+            min: "0",
+            value: state.maxBounces,
+            oninput: function (e: InputEvent) {
+              state.maxBounces = parseInt((e.target as HTMLInputElement).value);
+            },
+          }),
+        ]),
+        m(
+          "button",
+          { disabled: state.running, onclick: state.runRaytracing },
+          "Run raytracing",
+        ),
+      ]),
+      m(
+        "button",
+        {
+          disabled: state.audioToPlay === null,
+          onclick: state.playAudio,
+        },
+        "Play audio",
+      ),
+    ]);
+  },
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#run-raytracing")?.addEventListener("click", (e) =>
-    withDisabled(e.target as HTMLButtonElement, async () => {
-      audioToPlay =
-        (await rayTrace(RAY_TRACING_SETTINGS)) || new Float32Array();
-      const playBtn = document.querySelector("#play-audio");
-      if (playBtn instanceof HTMLButtonElement) {
-        playBtn.disabled = false;
-      }
-    }),
-  );
-
-  document.querySelector("#play-audio")?.addEventListener("click", (e) => {
-    ctx = ctx || new AudioContext();
-
-    const sourceBuffer = ctx.createBuffer(1, audioToPlay.length, SAMPLE_RATE);
-    const channel0 = sourceBuffer.getChannelData(0);
-    for (let i = 0; i < audioToPlay.length; ++i) {
-      channel0[i] = audioToPlay[i];
-    }
-
-    const source = ctx.createBufferSource();
-    source.buffer = sourceBuffer;
-    const gainNode = ctx.createGain();
-    gainNode.gain.value = 1;
-
-    source.connect(gainNode);
-    gainNode.connect(ctx.destination);
-    source.start(0);
-  });
+  const root = document.querySelector("#root");
+  if (root) {
+    m.mount(root, AppView);
+  }
 });
